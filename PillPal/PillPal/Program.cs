1087cb75c9cfe10aa6c90d05e61f79b61dfad88f
Uriel Fuentes <urielf@microsoft.com>
2025-09-19 16:04:56 -0600

    removing start code

--- PillPal/PillPal/Program.cs
+++ PillPal/PillPal/Program.cs
@@ -3,7 +3,6 @@ using Azure.AI.DocumentIntelligence;
 using Azure.AI.OpenAI;
 using OpenAI.Chat;
 
-//set `<your-endpoint>` and `<your-key>` variables with the values from the Azure portal to create your `AzureKeyCredential` and `DocumentIntelligenceClient` instance
 string endpoint = "https://ai-resource-vision.cognitiveservices.azure.com/";
 string key = "API_KEY";
 AzureKeyCredential credential = new AzureKeyCredential(key);
@@ -56,85 +55,3 @@ List<ChatMessage> messages = new List<ChatMessage>()
 
 var response = chatClient.CompleteChat(messages);
 System.Console.WriteLine(response.Value.Content[0].Text);
-
-//foreach (DocumentPage page in result.Pages)
-//{
-//	Console.WriteLine($"Document Page {page.PageNumber} has {page.Lines.Count} line(s), {page.Words.Count} word(s)," +
-//		$" and {page.SelectionMarks.Count} selection mark(s).");
-
-//	for (int i = 0; i < page.Lines.Count; i++)
-//	{
-//		DocumentLine line = page.Lines[i];
-
-//		Console.WriteLine($"  Line {i}:");
-//		Console.WriteLine($"    Content: '{line.Content}'");
-
-//		Console.Write("    Bounding polygon, with points ordered clockwise:");
-//		for (int j = 0; j < line.Polygon.Count; j += 2)
-//		{
-//			Console.Write($" ({line.Polygon[j]}, {line.Polygon[j + 1]})");
-//		}
-
-//		Console.WriteLine();
-//	}
-
-//	for (int i = 0; i < page.SelectionMarks.Count; i++)
-//	{
-//		DocumentSelectionMark selectionMark = page.SelectionMarks[i];
-
-//		Console.WriteLine($"  Selection Mark {i} is {selectionMark.State}.");
-//		Console.WriteLine($"    State: {selectionMark.State}");
-
-//		Console.Write("    Bounding polygon, with points ordered clockwise:");
-//		for (int j = 0; j < selectionMark.Polygon.Count; j++)
-//		{
-//			Console.Write($" ({selectionMark.Polygon[j]}, {selectionMark.Polygon[j + 1]})");
-//		}
-
-//		Console.WriteLine();
-//	}
-//}
-
-//for (int i = 0; i < result.Paragraphs.Count; i++)
-//{
-//	DocumentParagraph paragraph = result.Paragraphs[i];
-
-//	Console.WriteLine($"Paragraph {i}:");
-//	Console.WriteLine($"  Content: {paragraph.Content}");
-
-//	if (paragraph.Role != null)
-//	{
-//		Console.WriteLine($"  Role: {paragraph.Role}");
-//	}
-//}
-
-//foreach (DocumentStyle style in result.Styles)
-//{
-//	// Check the style and style confidence to see if text is handwritten.
-//	// Note that value '0.8' is used as an example.
-
-//	bool isHandwritten = style.IsHandwritten.HasValue && style.IsHandwritten == true;
-
-//	if (isHandwritten && style.Confidence > 0.8)
-//	{
-//		Console.WriteLine($"Handwritten content found:");
-
-//		foreach (DocumentSpan span in style.Spans)
-//		{
-//			var handwrittenContent = result.Content.Substring(span.Offset, span.Length);
-//			Console.WriteLine($"  {handwrittenContent}");
-//		}
-//	}
-//}
-
-//for (int i = 0; i < result.Tables.Count; i++)
-//{
-//	DocumentTable table = result.Tables[i];
-
-//	Console.WriteLine($"Table {i} has {table.RowCount} rows and {table.ColumnCount} columns.");
-
-//	foreach (DocumentTableCell cell in table.Cells)
-//	{
-//		Console.WriteLine($"  Cell ({cell.RowIndex}, {cell.ColumnIndex}) is a '{cell.Kind}' with content: {cell.Content}");
-//	}
-//}
